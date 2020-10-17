e5e727c8ee6d29418a996625a2ca0b9a036826ad
Gabriel Crespo <gabriel.crespo1942@gmail.com>
2020-10-17 18:17:29 -0300

    Terceiro dia - frontend finalizado

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -17,8 +17,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 const icon = L.icon({
     iconUrl: './public/images/map-marker.svg',
     iconSize: [58, 68],
-    iconAnchor: [29, 68],
-    popupAnchor: [170, 2]
+    iconAnchor: [29, 68]
 }) 
 
 // create and add marker
